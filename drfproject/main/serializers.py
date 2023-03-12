5d36d5f627340bfb1bc5f4aebeb1b2e1c894f4ec
034qwe <bnestercuk91@gmail.com>
2023-03-12 19:47:34 +0200

    serialiser.sSerialiser

--- drfproject/main/serializers.py
+++ drfproject/main/serializers.py
@@ -9,3 +9,18 @@ class ArticlesSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField(read_only=True)
     updated_at = serializers.DateTimeField(read_only=True)
     categ_id = serializers.IntegerField()
+
+
+    def create(self, validated_data):
+        return Articles.objects.create(**validated_data) 
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title',instance.title)
+        instance.anons = validated_data.get('anons',instance.anons)
+        instance.main_text = validated_data.get('main_text',instance.main_text)
+        instance.updated_at = validated_data.get('updated_at',instance.updated_at)
+        instance.categ_id = validated_data.get('categ_id',instance.categ_id)
+
+        instance.save()
+
+        return instance
