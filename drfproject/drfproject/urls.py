af59c6fd4be1e6d77d5bc435234b39b97a30c208
034qwe <bnestercuk91@gmail.com>
2023-03-13 15:49:53 +0200

    generics.RetrieveUpdateDestroyAPIView urls

--- drfproject/drfproject/urls.py
+++ drfproject/drfproject/urls.py
@@ -6,5 +6,6 @@ from main.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', ArticlesApiList.as_view(), name='articles'),
-    path('articles/<int:pk>/', ArticlesApiList.as_view(), name='article_detail'),
+    path('articles/<int:pk>/', ArticlesAPIUpdate.as_view(), name='article_put'),
+    path('articlesdetail/<int:pk>/', ArticlesAPIDetailView.as_view(), name='article_detail')
 ]
