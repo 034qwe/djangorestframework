8a182faa4a82d1526d33849eee10e7bd56cd6716
034qwe <bnestercuk91@gmail.com>
2023-03-13 11:33:19 +0200

    serializers.ModelSerializer

--- drfproject/main/serializers.py
+++ drfproject/main/serializers.py
@@ -2,25 +2,8 @@ from rest_framework import serializers
 from .models import Articles
 
 
-class ArticlesSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    anons =serializers.CharField()
-    main_text =serializers.CharField()
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
-    categ_id = serializers.IntegerField()
-
-
-    def create(self, validated_data):
-        return Articles.objects.create(**validated_data) 
+class ArticlesSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Articles
+        fields = ('title','anons','main_text','categ')
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title',instance.title)
-        instance.anons = validated_data.get('anons',instance.anons)
-        instance.main_text = validated_data.get('main_text',instance.main_text)
-        instance.updated_at = validated_data.get('updated_at',instance.updated_at)
-        instance.categ_id = validated_data.get('categ_id',instance.categ_id)
-
-        instance.save()
-
-        return instance
