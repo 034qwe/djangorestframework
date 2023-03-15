56f101be1244c1b5becb0cf95d5170a4dd993a0a
034qwe <bnestercuk91@gmail.com>
2023-03-15 21:12:58 +0200

    viewset

--- drfproject/main/views.py
+++ drfproject/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics ,viewsets
 from .serializers import *
 from rest_framework.views import Response
 from django.forms import model_to_dict
@@ -7,53 +7,25 @@ from rest_framework.views import APIView
 
 from .models import  Articles
 
-class ArticlesApiList(generics.ListCreateAPIView):
-    queryset = Articles.objects.all()
+# class ArticlesApiList(generics.ListCreateAPIView):
+#     queryset = Articles.objects.all()
+#     serializer_class = ArticlesSerializer
+
+
+
+# class ArticlesAPIUpdate(generics.UpdateAPIView):
+#     queryset = Articles.objects.all()
+#     serializer_class = ArticlesSerializer
+
+
+# class ArticlesAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Articles.objects.all()
+#     serializer_class = ArticlesSerializer
+
+class ArticlesViewSet(viewsets.ModelViewSet):
+    # queryset = Articles.objects.all()
     serializer_class = ArticlesSerializer
 
-
-
-class ArticlesAPIUpdate(generics.UpdateAPIView):
-    queryset = Articles.objects.all()
-    serializer_class = ArticlesSerializer
-
-
-class ArticlesAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Articles.objects.all()
-    serializer_class = ArticlesSerializer
-
-
-
-
-# class ArticlesAPIView(APIView):
-#     def get(self,request):
-#         w = Articles.objects.all()
-#         return Response({'posts':ArticlesSerializer(w,many=True).data})
-
-
-#     def post(self,request):
-#         serializer = ArticlesSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-
-
-#         return Response({'post': serializer.data})
-
-
-#     def put(self,request, *args,**kwargs):
-#         pk = kwargs.get('pk',None)
-#         if not pk:
-#             return Response({'error':'Method PUT not allowed'})
-        
-#         try:
-#             instance = Articles.objects.get(pk=pk)
-        
-#         except:
-#             return Response({'error':'Object does not exists'})
-
-#         serializer = ArticlesSerializer(data=request.data, instance=instance)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response({'post':serializer.data})
-
+    def get_queryset(self):
+        return Articles.objects.all()
 
