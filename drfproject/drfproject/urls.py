cfc5a3f98c07efdfa33341839b2157df5e889aa5
034qwe <bnestercuk91@gmail.com>
2023-03-15 21:12:25 +0200

    routers

--- drfproject/drfproject/urls.py
+++ drfproject/drfproject/urls.py
@@ -1,11 +1,17 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from main.views import *
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register('articles', ArticlesViewSet ,basename='articles')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('articles/', ArticlesApiList.as_view(), name='articles'),
-    path('articles/<int:pk>/', ArticlesAPIUpdate.as_view(), name='article_put'),
-    path('articlesdetail/<int:pk>/', ArticlesAPIDetailView.as_view(), name='article_detail')
-]
+    path('api/v1/',include(router.urls))
+#     path('articles/', ArticlesViewSet.as_view({'get':'list'}), name='articles'),
+#     path('articles/<int:pk>/', ArticlesViewSet.as_view({'put':'update'}), name='article_put'),
+#     path('articlesdetail/<int:pk>/', ArticlesViewSet.as_view(), name='article_detail')
+    ]
