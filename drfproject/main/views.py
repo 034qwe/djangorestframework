4fcaa121d0c49cf9e26effd06e754461ca612da9
034qwe <bnestercuk91@gmail.com>
2023-03-17 20:17:49 +0200

    -viewset  +permission classes

--- drfproject/main/views.py
+++ drfproject/main/views.py
@@ -4,28 +4,32 @@ from .serializers import *
 from rest_framework.views import Response
 from django.forms import model_to_dict
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from .permissions import IsOwnerOrReadOnly
+
 
 from .models import  Articles
 
-# class ArticlesApiList(generics.ListCreateAPIView):
-#     queryset = Articles.objects.all()
-#     serializer_class = ArticlesSerializer
-
-
-
-# class ArticlesAPIUpdate(generics.UpdateAPIView):
-#     queryset = Articles.objects.all()
-#     serializer_class = ArticlesSerializer
-
-
-# class ArticlesAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Articles.objects.all()
-#     serializer_class = ArticlesSerializer
-
-class ArticlesViewSet(viewsets.ModelViewSet):
-    # queryset = Articles.objects.all()
+class ArticlesApiList(generics.ListCreateAPIView):
+    queryset = Articles.objects.all()
     serializer_class = ArticlesSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
-    def get_queryset(self):
-        return Articles.objects.all()
+
+
+class ArticlesAPIUpdate(generics.UpdateAPIView):
+    queryset = Articles.objects.all()
+    serializer_class = ArticlesSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+class ArticlesAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Articles.objects.all() 
+    serializer_class = ArticlesSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+# class ArticlesViewSet(viewsets.ModelViewSet):
+#     # queryset = Articles.objects.all()
+#     serializer_class = ArticlesSerializer
+
+#     def get_queryset(self):
+#         return Articles.objects.all()
 
