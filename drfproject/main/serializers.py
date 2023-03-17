d8324166b3b8b6dbcb17f4ff824adb1a6e235789
034qwe <bnestercuk91@gmail.com>
2023-03-17 20:16:57 +0200

    exclude user from the form

--- drfproject/main/serializers.py
+++ drfproject/main/serializers.py
@@ -3,6 +3,9 @@ from .models import Articles
 
 
 class ArticlesSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+
     class Meta():
         model = Articles
         fields = '__all__'
