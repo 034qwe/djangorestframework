3a6efe39d78007cc4dd69e56ccedf86593eeafb0
034qwe <bnestercuk91@gmail.com>
2023-03-17 20:15:57 +0200

    delete, update login and logout urls

--- drfproject/drfproject/urls.py
+++ drfproject/drfproject/urls.py
@@ -5,13 +5,14 @@ from main.views import *
 from rest_framework import routers
 
 
-router = routers.SimpleRouter()
-router.register('articles', ArticlesViewSet ,basename='articles')
+# router = routers.SimpleRouter()
+# router.register('articles', ArticlesViewSet ,basename='articles')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include(router.urls))
-#     path('articles/', ArticlesViewSet.as_view({'get':'list'}), name='articles'),
-#     path('articles/<int:pk>/', ArticlesViewSet.as_view({'put':'update'}), name='article_put'),
-#     path('articlesdetail/<int:pk>/', ArticlesViewSet.as_view(), name='article_detail')
+    path('api/v1/articlesupdate/<int:pk>/',ArticlesAPIUpdate.as_view()),
+    path('api/v1/drf/', include('rest_framework.urls')),
+    path('api/v1/articlesdelete/<int:pk>/', ArticlesAPIDestroy.as_view()),
+    path('api/v1/articles/',ArticlesApiList.as_view())
+
     ]
