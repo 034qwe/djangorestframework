0b3b2e9b77889a9cb73870795ea467717a266b69
034qwe <bnestercuk91@gmail.com>
2023-03-19 21:02:10 +0200

    JWT Authentication urls

--- drfproject/drfproject/urls.py
+++ drfproject/drfproject/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path , include,re_path
 from main.views import *
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView,TokenVerifyView
 
 
 # router = routers.SimpleRouter()
@@ -10,11 +11,12 @@ from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/articlesupdate/<int:pk>/',ArticlesAPIUpdate.as_view()),
-    path('api/v1/drf/', include('rest_framework.urls')),
+    path('api/v1/articles/<int:pk>/',ArticlesAPIUpdate.as_view()),
     path('api/v1/articlesdelete/<int:pk>/', ArticlesAPIDestroy.as_view()),
     path('api/v1/articles/',ArticlesApiList.as_view()),
     path('api/v1/auth/',include('djoser.urls')),
-    re_path(r'^auth', include('djoser.urls.authtoken'))
-
+    re_path(r'^auth', include('djoser.urls.authtoken')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     ]
